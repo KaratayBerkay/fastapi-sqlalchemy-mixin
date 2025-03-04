3e4c1a297ddda8cb0eb45b23e59e7573a60b089c
berkay <karatay.berkay@gmail.com>
2025-03-04 12:55:44 +0300

    removed unc directories

--- app/app.py
+++ app/app.py
@@ -2,11 +2,28 @@ import routes
 
 from fastapi import FastAPI
 from fastapi import Request
+from fastapi.middleware.cors import CORSMiddleware
+from fastapi.staticfiles import StaticFiles
+
 from middlewares.token_middleware import token_middleware
 from controllers.route_controllers import RouteRegisterController
 
 
 app = FastAPI()
+app.mount("/static", StaticFiles(directory="static"), name="static")
+
+origins = [
+    "http://localhost",
+    "http://localhost:8080",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 route_register = RouteRegisterController(app=app, router_list=list(routes.__all__))
--- app/routes/auth/route.py
+++ app/routes/auth/route.py
@@ -1,14 +1,16 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Request, Response
 
 
 auth_route = APIRouter(prefix="/auth", tags=["auth"])
 
 
 @auth_route.get("/login", description="Login endpoint")
-async def login():
+async def login(request: Request, response: Response):
+    response.headers["X-Cat-Dog"] = "alone in the world"
     return {"message": "Login endpoint"}
 
 
 @auth_route.post("/register", description="Register endpoint")
-async def register():
+async def register(request: Request, response: Response):
+    response.headers["X-Cat-Dog"] = "alone in the world"
     return {"message": "Register endpoint"}
